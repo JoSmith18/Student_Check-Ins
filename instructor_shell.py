a56ae8097ae8311a0bf90efec7ebcce906315ae5
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2017-07-12 09:57:10 -0500

    defined choice and called function action from core using it as a parameter

--- instructor_shell.py
+++ instructor_shell.py
@@ -1,5 +1,13 @@
 from disk import checklog
+from core import action
 teacher = input("Which Instructor Will Check Today\n")
 
-print('Hello Mr.{} What Will You Be Checking Today?\n')
+print('Hello Mr.{} What Will You Be Checking Today?\n'.format(teacher))
 
+decision = '''
+\t1. All check in times
+\t2. Check in times for a particular day
+\t3. Check in times for a particular student
+'''
+choice = input(decision) 
+action(choice)
