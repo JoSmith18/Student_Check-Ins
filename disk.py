b625278cdf03f30b29ed7113a58735ec4b0caf16
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2017-07-12 11:13:12 -0500

    Added function in disk that read log and make check_in the lines

--- disk.py
+++ disk.py
@@ -8,3 +8,12 @@ def checkin(name):
 def checklog():
     with open("checkins.txt", "r") as file:
         print(file.read())
+
+def check_for_day(day):
+    with open("checkins.txt", "r") as log:
+        log.readline()
+        check_in = log.readlines()
+    times = []
+    for item in check_in:
+        if  item[0] == day
+    return item
