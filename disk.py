bc1ddfd0b41349e936b2ea53235a531bfee973db
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2017-07-12 09:47:36 -0500

    added a function so instructor could read all check in times

--- disk.py
+++ disk.py
@@ -3,3 +3,7 @@ from datetime import datetime
 def checkin(name):
     with open("checkins.txt","a") as log:
         log.write("{}, {}\n".format(datetime.today(), name))
+
+def checklog():
+    with open("checkins.txt", "r") as file:
+        print(file.read())
