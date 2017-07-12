ce09d5bdf6213d1cf040fdf85d972308de291106
Jo'Tavious Smith <jsmith@basecampcodingacademy.org>
2017-07-12 10:03:40 -0500

    Separated Date and Time

--- disk.py
+++ disk.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 def checkin(name):
     with open("checkins.txt","a") as log:
-        log.write("{}, {}\n".format(datetime.today(), name))
+        log.write("{}, {}\n".format(datetime.date(), name, datetime.time()))
 
 def checklog():
     with open("checkins.txt", "r") as file:
